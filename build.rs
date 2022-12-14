504643a73e66d932266928ef677c9487d2919b74
Blaquewithaq <blaquewithaq@gmail.com>
2022-12-14 16:31:07 -0600

    Pull submodule automatically

--- build.rs
+++ build.rs
@@ -1,9 +1,17 @@
-use std::env;
+use std::{env, path::Path, process::Command};
 
-// mod build_bindgen;
-// use crate::build_bindgen::bindgen;
+mod build_bindgen;
+use crate::build_bindgen::bindgen;
 
 fn main() {
+  println!("cargo:rerun-if-changed=build.rs");
+  println!("cargo:rerun-if-changed=wrapper.h");
+
+  if !Path::new("projectm/src").exists() {
+    let _ = Command::new("git")
+            .args(&["submodule", "update", "--init"])
+            .status();
+  }
 
   #[cfg(target_os = "windows")]
   let dst = cmake::build("projectm");
@@ -18,7 +26,6 @@ fn main() {
   let dst = cmake::build("projectm");
 
   println!("cargo:rustc-link-search=native={}/lib", dst.display());
-  // println!("cargo:rustc-link-arg=-sMIN_WEBGL_VERSION=2 -sMAX_WEBGL_VERSION=2");
 
   #[cfg(target_os = "windows")]
   if Ok("release".to_owned()) == env::var("PROFILE") {
@@ -48,7 +55,7 @@ fn main() {
     println!("cargo:rustc-link-lib=static=projectMd");
   }
   
-  // bindgen()
+  bindgen()
 }
 
 
