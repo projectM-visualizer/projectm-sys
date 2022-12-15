094e89474d86a9e028fc8563c8603a3a2fcee6bc
Blaquewithaq <blaquewithaq@gmail.com>
2022-12-14 19:01:57 -0600

    Added header picker based on enabled features

--- build_bindgen.rs
+++ build_bindgen.rs
@@ -5,10 +5,20 @@ pub fn bindgen() {
   
   let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
   let out_path = Path::new(".").join("src");
+  
+  // Get header based on features
+  fn get_header() -> String {
+    if cfg!(feature = "enable-playlist") {
+      return "bindgen/playlist.h".to_string().to_string()
+    } else {
+      return "bindgen/default.h".to_string().to_string()
+    }
+  }
+
   let bindings = bindgen::Builder::default()
-      .header("wrapper.h")
+      .header(get_header())
       .allowlist_function("projectm_.*")
-      .clang_arg(&format!("-I{}/include/libprojectM", out_dir.display()))
+      .clang_arg(&format!("-I{}/include", out_dir.display()))
       .parse_callbacks(Box::new(bindgen::CargoCallbacks))
       .generate()
       .expect("Unable to generate bindings");
